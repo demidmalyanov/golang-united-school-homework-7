786812db32dc2a781a0bdecb5455ccf976fff572
Demid <dagdog2506@gmail.com>
2022-06-30 17:05:24 +0500

    refactor: made table tests pattern

--- students_test.go
+++ students_test.go
@@ -21,72 +21,108 @@ func init() {
 }
 
 // WRITE YOUR CODE BELOW
+type testCases struct{
+	descr string
+	input People
+	want interface{}
+}
+
+type testCaseMatrix struct{
+	descr string
+	input *Matrix
+	matrixStr string
+}
+
+type testCaseRowsCols struct{
+	descr string
+	input [][]int
+	matrix string
+}
 
 func TestLen(t *testing.T){
 
+	t.Parallel()
 
-	p := Person{"John","Dow",time.Now()}
+	p := Person{}
 
-	
-	type test struct{
-		input People
-		want int
-	}
+	for _,tcase := range []testCases{
+		{
+			descr: "1 person",
+			input: People{p},
+			want:1,
 
-	tests := []test{
-		{input: People{p,p,p},want: 3},
-		{input: People{p,p,p,p},want: 4},
-		{input: People{},want: 0},
-	}
-
-
-	for _,test := range tests{
-		got := test.input.Len()
-
-		if got != test.want{
-			t.Errorf("Expected want %d , but got %d",test.want,got)
-		}
-
+		},
+		{
+			descr: "4 persons",
+			input: People{p,p,p,p},
+			want: 4,
+		},
+	}{
+		t.Run(tcase.descr,func(t *testing.T){
+			got := tcase.input.Len()
+			if got != tcase.want{
+				t.Errorf("Expected %v, but got %v",tcase.want,got)
+			}
+		})
 	}
 }
 
 func TestLess(t *testing.T){
 
-	type test struct{
-		input People
-		want bool
-	}
+	t.Parallel()
 
-
+	//Test data
 	p1 := Person{"John","Down",time.Date(1999, 8, 2, 0, 0, 0, 0, time.UTC)}
 	p2 := Person{"John","Grayhouse",time.Date(1999, 8, 2, 0, 0, 0, 0, time.UTC)}
 	p3 := Person{"Gregory","Downtown",time.Date(1999, 8, 2, 0, 0, 0, 0, time.UTC)}
 	p4 := Person{"Antony","Woodpecker",time.Date(2014, 2, 1, 0, 0, 0, 0, time.UTC)}
 
-	tests := []test{
-		{input: People{p1,p4},want: true},
-		{input: People{p4,p1},want: false},
-		{input: People{p1,p3},want: true},
-		{input: People{p3,p1},want: false},
-		{input: People{p1,p2},want: false},
-		{input: People{p2,p1},want: true},
+	for _,tcase := range []testCases{
+		{
+			descr: "First is older",
+			input: People{p1,p4},
+			want: true,
+		},
+		{
+			descr: "First is younger",
+			input: People{p4,p1},
+			want: false,
+		},
+		{
+			descr: "First is older,name is smaller",
+			input: People{p1,p3},
+			want: true,
+		},
+		{
+			descr: "First is younger,name is bigger",
+			input: People{p3,p1},
+			want: false,
+		},
+		{
+			descr: "Same age, same name, last name is smaller",
+			input: People{p1,p2},
+			want: false,
+		},
+		{
+			descr: "Same age, same name, last name is bigger",
+			input: People{p2,p1},
+			want: true,
+		},
+	}{
+		t.Run(tcase.descr,func(t *testing.T) {
+			got := tcase.input.Less(1,0)
+			if got != tcase.want{
+				t.Errorf("Expected %t, but got %t",tcase.want,got)
+			}
+		})
 	}
-
-	for _,test := range tests{
-		
-
-		if test.input.Less(1,0) != test.want{
-			t.Errorf("Expected %t, but got %t",test.want,test.input.Less(1,0))
-		}
-		
-
-	}
-
 }
 
 
 func TestSwap(t *testing.T){
-
+	t.Parallel()
+	
+	// Test data
 	p1:= Person{"John","Dow",time.Now()}
 	p2:= Person{"Greg","Frank",time.Now()}
 
@@ -98,93 +134,119 @@ func TestSwap(t *testing.T){
 		t.Errorf("Person 2 expected to be first element.")
 	}
 
-
 }
 
-
 func TestNewMatrix(t *testing.T){
+	t.Parallel()
 
-	type test struct{
-		name string
-		input string
-		cols,rows int
-		data []int
-	}
-
-	validCases := []test{
-		{"basic matrix 3x3","5 6 7\n 7 7 7",3,3,[]int{5,6,7,7,7,7}},
-		{"matrix 2x2","4 6\n6 7",2,2,[]int{4,6,6,7}},
-		{"matrix with spaces"," 5 6 \n 6 1",2,2,[]int{5,6,6,1}},
-
+	for _,tcase := range []testCaseMatrix{
+		{
+			descr:"basic matrix 3x3",
+			input:&Matrix{3,3,[]int{5,6,7,7,7,7,7,6,7}},
+			matrixStr:"5 6 7\n7 7 7\n7 6 7",
+		},
+		{
+			descr:"matrix 2x2",
+			input: &Matrix{2,2,[]int{4,6,6,7}},
+			matrixStr:"4 6\n6 7",
+		},
+		{
+			descr:"matrix with spaces",
+			input: &Matrix{2,2,[]int{5,6,6,1}},
+			matrixStr:" 5 6 \n 6 1",
+		},
 		
+	}{
+		t.Run(tcase.descr,func(t *testing.T) {
+			got,_ := New(tcase.matrixStr)
+			assert.Equal(t,got.data,tcase.input.data)
+		})
 	}
-	errorCases :=[]test{
-		{"rows>cols","5 4\n6 4 5",2,3,[]int{5,5,5}},
-		{"wrong format of values","6 h 5\n7 7 7",3,2,[]int{4,5,6}},
-		{"cols>rows","4 5 6\n5 6",3,3,[]int{5}},
-		{"empty matrix","",0,0,[]int{6,6,4}},
+}
 
-	}
+func TestNewMatrixWithErrors(t *testing.T){
+	t.Parallel()
 
-	for _,validTest := range validCases{
-		m,_ := New(validTest.input)
-
-		if len(m.data) != len(validTest.data){
-			t.Errorf("Error in case %s",validTest.name)
-		}	
-	}
-
-	for _,ererrorCases := range errorCases{
-		_,err := New(ererrorCases.input)
-		if err != nil{
-			continue
-		}
+	for _, tcase := range []testCaseMatrix{
+		{
+			descr:"rows>cols",
+			input: &Matrix{2,3,[]int{5,5,5}},
+			matrixStr:"5 4\n6 4 5",
+		},
+		{
+			descr:"wrong format of values",
+			input: &Matrix{3,2,[]int{4,5,6}},
+			matrixStr:"6 h 5\n7 7 7",
+		},
+		{
+			descr:"cols>rows",
+			input: &Matrix{3,2,[]int{5}},
+			matrixStr:"4 5 6\n5 6",
+		},
+		{
+			descr:"empty matrix",
+			input: &Matrix{0,0,[]int{}},
+			matrixStr:"",
+		},
+	}{
+		t.Run(tcase.descr,func(t *testing.T) {
+			_ ,err := New(tcase.matrixStr)
+			assert.NotNil(t,err)
+		})
 	}
 }
 
 
 func TestMatrixRows(t *testing.T){
+	t.Parallel()
 
-	type test struct{
-		name string
-		input string
-		expectedRows [][]int
-	}
+	for _, tcase := range []testCaseRowsCols{
 
-	testCases := []test{
-		{"1 row","5 7 1",[][]int{{5,7,1}}},
-		{"multiple rows","5 7 1\n7 8 6",[][]int{{5,7,1},{7,8,6}}},
-	}
-
-	for _,testCase := range testCases{
-
-		m,_ := New(testCase.input)
-		assert.Equal(t,testCase.expectedRows,m.Rows())
+		{
+			descr: "1 row",
+			input: [][]int{{4,5,4}},
+			matrix: "4 5 4",
+		},
+		{
+			descr: "multiple rows",
+			input: [][]int{{7, 5, 5}, {6, 4, 5}, {9, 9, 9}},
+			matrix: "7 5 5\n6 4 5\n9 9 9",
+		},
+	}{
+		t.Run(tcase.descr,func(t *testing.T) {
+			m, _ := New(tcase.matrix)
+			assert.Equal(t,m.Rows(),tcase.input)
+		})
 	}
 }
 
 func TestMatrixCols(t *testing.T){
+	t.Parallel()
 
-	type test struct{
-		name string
-		input string
-		expectedRows [][]int
-	}
+	for _, tcase := range []testCaseRowsCols{
 
-	testCases := []test{
-		{"1 col","5 7 1",[][]int{{5},{7},{1}}},
-		{"multiple col","5 7 1\n7 8 6",[][]int{{5,7},{7,8},{1,6}}},
-	}
-
-	for _,testCase := range testCases{
-
-		m,_ := New(testCase.input)
-		assert.Equal(t,testCase.expectedRows,m.Cols())
+		{
+			descr: "1 col",
+			input: [][]int{{4},{5},{4}},
+			matrix: "4 5 4",
+		},
+		{
+			descr: "multiple cols",
+			input: [][]int{{7,6,9},{5,4,9},{5,5,9}},
+			matrix: "7 5 5\n6 4 5\n9 9 9",
+		},
+	}{
+		t.Run(tcase.descr,func(t *testing.T) {
+			m, _ := New(tcase.matrix)
+			assert.Equal(t,m.Cols(),tcase.input)
+		})
 	}
 }
 
 
 func TestMatrixSet(t *testing.T){
+
+	t.Parallel()
 
 	type test struct{
 		name string
@@ -204,8 +266,6 @@ func TestMatrixSet(t *testing.T){
 		{"col out of range","5 5 7\n6 5 7",1,5,0},
 	}
 
-
-
 	for _,testCase := range testCases{
 		m,_ := New(testCase.input)
 		assert.True(t,m.Set(testCase.row,testCase.col,testCase.value))
@@ -220,3 +280,4 @@ func TestMatrixSet(t *testing.T){
 		}
 	}
 }
+
